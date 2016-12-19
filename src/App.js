2d1d61820595fc1944b61fb239b5bd4ef21a63bb
Alexander Demin <bouvens@gmail.com>
2016-12-19 18:37:47 +0600

    Initial commit for boilerplate project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { PropTypes, Component } from 'react'
+import './App.css'
+
+class ZeroPacker extends Component {
+    render () {
+        return (
+            <form className="encoder">
+                There will be text
+            </form>
+        )
+    }
+}
+
+const App = () => (
+    <ZeroPacker/>
+)
+
+export default App
