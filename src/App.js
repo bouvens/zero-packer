e9f60f55cf36f06da670db4af1c6b5adaab2176c
Alexander Demin <bouvens@gmail.com>
2017-02-14 00:36:14 +0600

    <SettersBlock /> extracted, params moved to App

--- src/App.js
+++ src/App.js
@@ -2,8 +2,59 @@ import React from 'react'
 import ZeroPacker from './components/ZeroPacker'
 import './App.css'
 
+const DEFAULTS = {
+    leader: '0',
+    symbols: [
+        0x2060,
+        0x2061,
+        0x2062,
+        0x2063,
+        0x180E,
+        0x200B,
+        0x200C,
+        0x200D,
+        0xFEFF,
+    ].map((uCode) => String.fromCharCode(uCode)).join(''),
+    textToPacked: '“test”',
+    packedToText: '',
+}
+
+const SETTERS = [
+    {
+        params: {
+            leader: DEFAULTS.leader,
+            symbols: DEFAULTS.symbols,
+        },
+        text: 'Zero-width symbols with leading zero'
+    },
+    {
+        params: {
+            leader: '',
+            symbols: '0123456789,',
+        },
+        text: 'Comma separated unicode codes'
+    },
+    {
+        params: {
+            leader: '',
+            symbols: '↑↓←→BA ',
+        },
+        text: 'Arbitrary symbols'
+    },
+    {
+        params: {
+            leader: '',
+            symbols: '·•×⌀',
+        },
+        text: 'More'
+    },
+]
+
 const App = () => (
-    <ZeroPacker />
+    <ZeroPacker
+        defaults={DEFAULTS}
+        setters={SETTERS}
+    />
 )
 
 export default App
--- src/components/ZeroPacker.jsx
+++ src/components/ZeroPacker.jsx
@@ -1,95 +1,35 @@
 import React, { PropTypes } from 'react'
 import { encodeToSymbols, decodeFromSymbols } from '../coder'
+import { SettersBlock } from './SettersBlock'
 import { LabeledConnector, LabeledInput } from './LabeledControl'
 import './ZeroPacker.css'
 
-const DEFAULTS = {
-    leader: '0',
-    symbols: [
-        0x2060,
-        0x2061,
-        0x2062,
-        0x2063,
-        0x180E,
-        0x200B,
-        0x200C,
-        0x200D,
-        0xFEFF,
-    ].map((uCode) => String.fromCharCode(uCode)).join(''),
-    textToPacked: '“test”',
-    packedToText: '',
-}
-const IDS = {
-    leader: 'leader',
-    symbols: 'symbols',
-    textToPacked: 'textToPacked',
-    packedFromText: 'packedFromText',
-    packedToText: 'packedToText',
-    textFromPacked: 'textFromPacked',
-}
-const SETTERS = [
-    {
-        params: {
-            leader: DEFAULTS.leader,
-            symbols: DEFAULTS.symbols,
-        },
-        text: 'Zero-width symbols with leading zero'
-    },
-    {
-        params: {
-            leader: '',
-            symbols: '0123456789,',
-        },
-        text: 'Comma separated unicode codes'
-    },
-    {
-        params: {
-            leader: '',
-            symbols: '↑↓←→BA ',
-        },
-        text: 'Arbitrary symbols'
-    },
-    {
-        params: {
-            leader: '',
-            symbols: '·•×⌀',
-        },
-        text: 'More'
-    },
-]
-
-const Setter = (props) => (
-    <div className="setter">
-        <a onClick={props.onClick(props.params)} tabIndex={props.tabIndex}>{props.text}</a>
-    </div>
-)
-
-Setter.propTypes = {
-    params: PropTypes.object,
-    text: PropTypes.string,
-    tabIndex: PropTypes.number,
-    onClick: PropTypes.func,
-}
-
 export default class ZeroPacker extends React.Component {
     static propTypes = {
-        defaults: PropTypes.object,
-    }
-    static defaultProps = {
-        defaults: DEFAULTS,
+        defaults: PropTypes.object.isRequired,
+        setters: PropTypes.array.isRequired,
     }
 
     state = this.props.defaults
+
+    IDS = {
+        leader: 'leader',
+        symbols: 'symbols',
+        textToPacked: 'textToPacked',
+        packedFromText: 'packedFromText',
+        packedToText: 'packedToText',
+        textFromPacked: 'textFromPacked',
+    }
 
     changeHandler = (state) => (event) => {
         const initialValue = event.target.value
         let value
 
         switch (state) {
-            case IDS.leader:
+            case this.IDS.leader:
                 value = initialValue.slice(-1)
                 break
-            case IDS.symbols:
+            case this.IDS.symbols:
                 value = initialValue
                     .split('')
                     .filter((char, i, arr) => arr.indexOf(char) === i && char !== this.state.leader)
@@ -107,7 +47,7 @@ export default class ZeroPacker extends React.Component {
     getDecoded = () => decodeFromSymbols(this.state.packedToText, this.getSymbols(), this.state.leader)
 
     move = () => {
-        this.setState({ [IDS.packedToText]: this.getEncoded() })
+        this.setState({ [this.IDS.packedToText]: this.getEncoded() })
     }
 
     selectAll = (_this) => () => _this.control.setSelectionRange(0, _this.control.value.length)
@@ -115,38 +55,34 @@ export default class ZeroPacker extends React.Component {
     render () {
         return (
             <div className="encoder">
-                {SETTERS.map((setter, index) => (
-                    <Setter
-                        onClick={(state) => () => this.setState(state)}
-                        key={index}
-                        tabIndex={index + 1}
-                        {...setter}
-                    />
-                ))}
+                <SettersBlock
+                    setters={this.props.setters}
+                    setState={(state) => this.setState(state)}
+                />
                 <LabeledConnector
                     state={this.state}
                     onChange={this.changeHandler}
                     onFocus={this.selectAll}
                 >
                     <LabeledInput
-                        id={IDS.leader}
+                        id={this.IDS.leader}
                         label="Leading symbol"
                         className="leader"
                     />
                     <div className="arrow">+</div>
                     <LabeledInput
-                        id={IDS.symbols}
+                        id={this.IDS.symbols}
                         label="Symbols for packing"
                         className="symbols"
                     />
                     <LabeledInput
-                        id={IDS.textToPacked}
+                        id={this.IDS.textToPacked}
                         label="Text"
                         multiLine
                     />
                     <div className="arrow">→</div>
                     <LabeledInput
-                        id={IDS.packedFromText}
+                        id={this.IDS.packedFromText}
                         label="Packed"
                         value={this.getEncoded()}
                         readOnly
@@ -154,13 +90,13 @@ export default class ZeroPacker extends React.Component {
                     />
                     <button className="move" onClick={this.move}>Move packed ↓</button>
                     <LabeledInput
-                        id={IDS.packedToText}
+                        id={this.IDS.packedToText}
                         label="Packed"
                         multiLine
                     />
                     <div className="arrow">→</div>
                     <LabeledInput
-                        id={IDS.textFromPacked}
+                        id={this.IDS.textFromPacked}
                         label="Text"
                         value={this.getDecoded()}
                         readOnly
